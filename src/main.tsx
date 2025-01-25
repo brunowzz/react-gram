942100a6b9322120a5743a612f1652718f5b1d1d
Bruno Barreiras <brunooliver368@gmail.com>
2025-01-25 17:03:16 -0300

    chore: add redux config

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
 import Routes from "./routes/routes.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Routes />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   </StrictMode>
 );
